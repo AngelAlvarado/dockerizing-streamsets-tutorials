8db4728794ef615e0428eeff57009868bb159eb1
Angel Alvarado <angel.angelio@gmail.com>
2016-09-04 15:27:43 -0400

    fixed version of images and enabling geohash for ElasticSearch index

--- build/start.sh
+++ build/start.sh
@@ -2,6 +2,7 @@
 
 cd /build
 sh /start
-curl -XPUT 'http://elasticsearch:9200/logs' -d '{"mappings": {"logs" : {"properties" : {"timestamp":{"type": "date"},"geo": {"type": "geo_point"},"city": {"type": "string","index": "not_analyzed"}}}}}
+curl -XPUT 'http://elasticsearch:9200/logs' -d '{"mappings": {"logs" : {"properties" : {"timestamp":{"type": "date"},"geo": {"type": "geo_point", "geohash": true},"city": {"type": "string","index": "not_analyzed"}}}}}'
+
 
 
